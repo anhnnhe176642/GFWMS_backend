c9e69e00be30f94738f613e31e074fdc3cf1831a
anhnnhe176642 <anhnnhe176642@fpt.edu.vn>
2025-10-19 13:59:54 +0700

    refactor: improve server log message

--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ import app from "./app.js";
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`server is running on \x1b[36mhttp://localhost:${PORT}/api-docs\x1b[0m`);
+  console.log(`server is running...\napi docs: \x1b[36mhttp://localhost:${PORT}/api-docs\x1b[0m`);
 });
