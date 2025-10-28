5c368b6e6c2a8d57e06cedc45a477f89bcc4688b
AnhNN <anhnnhe1766422@fpt.edu.vn>
2025-10-28 17:29:10 +0700

    refactor(user.routes): remove unused pagination query schema import

--- src/routes/v1/user.routes.js
+++ src/routes/v1/user.routes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { getAllUsers, createUser, getUserById, updateUserStatus, updateUserRole, deleteUser } from '../../controllers/user.controller.js';
 import { authenticateToken, requirePermission, requireOwnershipOrPermission } from '../../middlewares/auth.middleware.js';
 import { validate, validateMultiple } from '../../middlewares/validation.middleware.js';
-import { createUserSchema, updateUserStatusSchema, updateUserRoleSchema, uuidParamSchema, paginationQuerySchema, userQuerySchema } from '../../validations/user.validation.js';
+import { createUserSchema, updateUserStatusSchema, updateUserRoleSchema, uuidParamSchema, userQuerySchema } from '../../validations/user.validation.js';
 import { PERMISSIONS } from '../../constants/permissions.js';
 
 const router = express.Router();
