a8c2d5ffb4e7dc7265b3ef26ca2fb6accc435001
anhnnhe176642 <anhnnhe176642@fpt.edu.vn>
2025-12-16 12:52:47 +0700

    refactor: remove markdown plugin configuration from ESLint setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
 import js from "@eslint/js";
 import globals from "globals";
 import json from "@eslint/json";
-import markdown from "@eslint/markdown";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
@@ -33,11 +32,5 @@ export default defineConfig([
     plugins: { json },
     language: "json/json5",
     extends: ["json/recommended"],
-  },
-  {
-    files: ["**/*.md"],
-    plugins: { markdown },
-    language: "markdown/gfm",
-    extends: ["markdown/recommended"],
   },
 ]);
